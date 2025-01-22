05146f666b65bb8914550f73d8b698921fe87766
Akhil-ps-dev <psakhil211@gmail.com>
2025-01-22 12:08:37 +0530

    data fetched

---
+++ lib/core/const/ksize.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class KSizedBox {
+  static const kDefSizedBoxH5 = SizedBox(
+    height: 5,
+  );
+
+  static const kDefSizedBoxH10 = SizedBox(
+    height: 10,
+  );
+
+  static const kDefSizedBoxH20 = SizedBox(
+    height: 20,
+  );
+
+  static const kDefSizedBoxW10 = SizedBox(
+    width: 10,
+  );
+}
+
+//border Radius
+final kBorderR10 = BorderRadius.circular(10);
